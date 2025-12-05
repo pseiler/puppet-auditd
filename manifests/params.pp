b393138bcacedc2cac11630e47a8a15a2bbe5c9e
Ludwig Cornelius <t_d_l_c@gmx.de>
2025-12-05 12:40:31 +0100

    audisp rsyslog support for sles sp4 and higher

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,29 +2,27 @@ class auditd::params {
   # OS specific variables.
   case $facts['os']['family'] {
     'Suse': {
-      $package_name             = 'audit'
-      $audisp_dir               = '/etc/audisp'
-      $disp_qos                 = 'lossy'
-      $dispatcher               = '/sbin/audispd'
-      $auditd_audisp_syslog_path = 'builtin_syslog'
-      $auditd_audisp_syslog_type = 'builtin'
+      $package_name              = 'audit'
+      $audisp_package            = 'audit-audispd-plugins'
+      $manage_audit_files        = true
+      $rules_file                = '/etc/audit/rules.d/puppet.rules'
 
       # Starting with SLES15 SP4 it uses auditd >= 3.0 and has no audisp configuration anymore.
       if versioncmp($facts['os']['release']['full'], '15.4') >= 0 {
         $has_audisp_config = false
+        $audisp_dir                = '/etc/audit'
+        $disp_qos                  = undef
+        $dispatcher                = undef
+        $auditd_audisp_syslog_path = '/sbin/audisp-syslog'
+        $auditd_audisp_syslog_type = 'always'
       }
       else {
         $has_audisp_config = true
-      }
-      if versioncmp($facts['os']['release']['major'], '12') >= 0 {
-        $audisp_package     = 'audit-audispd-plugins'
-        $manage_audit_files = true
-        $rules_file         = '/etc/audit/rules.d/puppet.rules'
-      }
-      else {
-        $audisp_package     = 'audispd-plugins'
-        $manage_audit_files = true
-        $rules_file         = '/etc/audit/audit.rules'
+        $audisp_dir                = '/etc/audisp'
+        $disp_qos                  = 'lossy'
+        $dispatcher                = '/sbin/audispd'
+        $auditd_audisp_syslog_path = 'builtin_syslog'
+        $auditd_audisp_syslog_type = 'builtin'
       }
     }
     'RedHat': {
